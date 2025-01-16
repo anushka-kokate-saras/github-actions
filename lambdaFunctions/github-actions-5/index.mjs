86d827a235aab2848aab6d305cbeebd5e2625395
anushka-kokate-saras <anushka@saraslabs.co>
2025-01-16 09:30:51 +0530

    added 5th lambda

---
+++ lambdaFunctions/github-actions-5/index.mjs
@@ -0,0 +1,9 @@
+export const handler = async (event) => {
+    // TODO implement
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify('Hello from Lambda! Deployed using Github Actions, Modified config details 4'),
+    };
+    return response;
+  };
+  
